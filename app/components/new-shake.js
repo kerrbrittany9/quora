059f97b009fd3a9a5560b0a50a0f2c65c6e9a7e6
brittany kerr <kerrbrittany9@gmail.com>
2017-08-11 10:36:44 -0700

    write new-shake form on component new-shake, save to index

---
+++ app/components/new-shake.js
@@ -0,0 +1,20 @@
+import Ember from 'ember';
+
+export default Ember.Component.extend({
+  addNewShake: false,
+   actions: {
+     shakeFormShow() {
+       this.set('addNewShake', true);
+     },
+
+     saveShake() {
+       var params = {
+         question: this.get('question'),
+         author: this.get('author'),
+         notes: this.get('notes'),
+       };
+       this.set('addNewShake', false);
+       this.sendAction('saveShake', params);
+     }
+   }
+ });
--- app/routes/index.js
+++ app/routes/index.js
@@ -4,4 +4,12 @@ export default Ember.Route.extend({
   model() {
     return this.store.findAll('shake');
   },
+
+  actions: {
+  saveShake(params) {
+    var newShake = this.store.createRecord('shake', params);
+    newShake.save();
+    this.transitionTo('index');
+    }
+  }
 });
