ccfe7589fcf89fc76a184f550340e8520e1840f1
brittany kerr <kerrbrittany9@gmail.com>
2017-08-13 19:11:14 -0700

    write update hook into shake tile js

--- app/components/shake-detail.js
+++ app/components/shake-detail.js
@@ -4,6 +4,16 @@ export default Ember.Component.extend({
   actions: {
   update(response, params) {
       this.sendAction('update', response, params);
-    }
+    },
+
+    update(shake, params) {
+      Object.keys(params).forEach(function(key) {
+        if(params[key]!==undefined) {
+          shake.set(key,params[key]);
+        }
+      });
+      shake.save();
+      this.transitionTo('index');
+      }
   }
 });
--- app/components/shake-tile.js
+++ app/components/shake-tile.js
@@ -1,5 +1,15 @@
 import Ember from 'ember';
 
 export default Ember.Component.extend({
-
+  actions: {
+  update(shake, params) {
+    Object.keys(params).forEach(function(key) {
+      if(params[key]!==undefined) {
+        shake.set(key,params[key]);
+      }
+    });
+    shake.save();
+    this.transitionTo('index');
+    }
+  }
 });
